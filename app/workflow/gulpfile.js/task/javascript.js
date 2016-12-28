d72a4175992a313c5122dd0f39da82bd43cf4971
Stefan Kuijers <stefan@arteries.hu>
2016-12-28 14:40:38 +0100

    #43 | babel presets | using local require

--- app/workflow/gulpfile.js/task/javascript.js
+++ app/workflow/gulpfile.js/task/javascript.js
@@ -1,13 +1,14 @@
 /* Sass */
 
 module.exports = function (gulp, plugin, config) {
-    return function (source) {
+    const latestPreset = require('babel-preset-latest'); 
 
+    return function (source) {
         return gulp.src(typeof(source) === 'string' ? source : config.javascript.globs)
             .pipe( plugin.sourcemaps.init() )
             .pipe(plugin.plumber( {errorHandler: config.error.handler}) )
             .pipe(plugin.babel({
-            	presets: "latest"
+            	presets: latestPreset
             }))
             .pipe(plugin.concat('script.min.js'))
             .pipe(plugin.uglify())
